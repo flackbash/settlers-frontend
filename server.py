02873c45af5a6f277568800a2d1daa5bd3633582
Natalie Prange <flack2bash@gmail.com>
2021-01-03 18:50:52 +0100

    Implement dice roll, draw&discard card
    and add hover optics.
    I promise I'll start making properly fine-grained commits.

--- server.py
+++ server.py
@@ -1,13 +1,60 @@
 import argparse
-from flask import Flask, render_template
+import json
 
+from flask import Flask, render_template, request
+
+from settlers_server.src.colonization.dice import roll_two_dice
+from settlers_server.src.colonization.table import Table
 
 app = Flask(__name__)
+
+default_response = json.dumps('{"test": "ok"}')
+
+table = Table(["p1", "p2", "p3"])
 
 
 @app.route("/")
 def home():
     return render_template("settlers.html")
+
+
+@app.route('/get_dice_roll')
+def get_dice_roll():
+    result1, result2 = roll_two_dice()
+    json_response = json.dumps({"result1": result1, "result2": result2})
+    return json_response
+
+
+@app.route('/draw_development_card')
+def draw_development_card():
+    player_id = request.args.get("player")
+    text = table.draw_development_card(player_id)
+    response = dict()
+    response["development_card"] = dict()
+    response["development_card"]["player_id"] = player_id
+    response["development_card"]["text"] = text
+
+    json_response = json.dumps(response)
+    return json_response
+
+
+@app.route('/draw_resource_card')
+def draw_resource_card():
+    player_id = request.args.get("player_id")
+    resource_type = request.args.get("type")
+    print("player_id: ", player_id)
+    print("type: ", resource_type)
+    table._hands[player_id].add_resource_card(resource_type)
+    return json.dumps({"success": "true"})
+
+
+@app.route('/discard_resource_card')
+def discard_resource_card():
+    player_id = request.args.get("player_id")
+    resource_type = request.args.get("type")
+    print("player_id: ", player_id)
+    print("type: ", resource_type)
+    return json.dumps({"success": "true"})
 
 
 if __name__ == "__main__":
--- static/settlers.js
+++ static/settlers.js
@@ -1,23 +1,110 @@
 // ---------------------- Global variables -------------------------------------
+// URL building
 var basePath = window.location.pathname.replace(/\/$/, "") + "/";
-var URL_PREFIX_DRAW_RESOURCE = basePath + "draw_resource_card?";
+var URL_DRAW_RESOURCE = basePath + "draw_resource_card";
+var URL_DISCARD_RESOURCE = basePath + "discard_resource_card";
+var URL_ROLE_DICE = basePath + "get_dice_roll";
 
+// Player variables
 // TODO: Adjust player id. Where to store it?
-var player_id = 0;
+var player_id = "p1";
 
 /*
- * Send request to server to draw a resource card
+ * Send request to server to draw a resource card and display result.
  */
-function drawResource(type) {
-  var url = URL_PREFIX_QAC + type;
+function drawResourceCard(type) {
+  var params = {"type": type, "player_id": player_id};
+  params = $.param(params);
+
+  var url = URL_DRAW_RESOURCE + "?" + params;
+
   $.getJSON(url, function(jsonObj) {
-    // Bail early if the result is empty
     if (jsonObj.length == 0) return;
 
     var success = jsonObj["success"];
 
-    if (success == true) {
-        console.log("Drew a card of type " + type);
+    if (success == "true") {
+        // Increase the resource card number of the corresponding type by one
+        var numberObject = $('.resource-card.' + type + ' .number');
+        var currNumber = parseInt(numberObject.html());
+        numberObject.html(currNumber + 1);
     }
-  }
+  });
+}
+
+/*
+ * Send request to server to discard a resource card and display result.
+ */
+function discardResourceCard(type) {
+  var params = {"type": type, "player_id": player_id};
+  params = $.param(params);
+
+  var url = URL_DISCARD_RESOURCE + "?" + params;
+
+  $.getJSON(url, function(jsonObj) {
+    if (jsonObj.length == 0) return;
+
+    var success = jsonObj["success"];
+
+    if (success == "true") {
+        // Decrease the resource card number of the corresponding type by one
+        var numberObject = $('.resource-card.' + type + ' .number');
+        var currNumber = parseInt(numberObject.html());
+        if (currNumber > 0) {
+            numberObject.html(currNumber - 1);
+        }
+    }
+  });
+}
+
+/*
+ * Send request to server to role the dice and display result.
+ */
+function roleDice() {
+  var url = URL_ROLE_DICE;
+
+  $.getJSON(url, function(jsonObj) {
+    if (jsonObj.length == 0) return;
+
+    var result1 = jsonObj["result1"];
+    var result2 = jsonObj["result2"];
+
+    // Display the dice roll
+    var die1 = $('#die1');
+    var img1 = getDieImageUrl(result1);
+    console.log(img1);
+    die1.css('background-image', 'url(' + img1 + ')');
+    var die2 = $('#die2');
+    var img2 = getDieImageUrl(result2);
+    die2.css('background-image', 'url(' + img2 + ')');
+  });
+}
+
+/*
+ * For a given number between 1 and 6 retrieve the correct die image url.
+ */
+function getDieImageUrl(number) {
+    var baseUrl = "static/img/"
+    var filename = "";
+    switch (number) {
+        case 1:
+          filename = "one.png";
+          break;
+        case 2:
+          filename = "two.png";
+          break;
+        case 3:
+            filename = "three.png";
+          break;
+        case 4:
+          filename = "four.png";
+          break;
+        case 5:
+          filename = "five.png";
+          break;
+        case 6:
+          filename = "six.png";
+          break;
+    }
+    return baseUrl + filename;
 }
